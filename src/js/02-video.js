22e4756f141311662d9259281486874733e99455
Evhenii Danchenko <danchenkoevhenii@gmail.com>
2022-07-13 00:34:59 +0300

    add task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+let LOCALSTORAGE_KEY = 'videoplayer-current-time'
+
+player.on('timeupdate', throttle(setSeconds, 1000));
+
+function setSeconds({ seconds }) {
+    localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+}
+
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY))
